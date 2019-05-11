364dbc0f55d7c895cdadc4dee7ce8b3cdbff7bd0
Eguchi3795 <50046336+Eguchi3795@users.noreply.github.com>
2019-05-11 20:20:35 +0900

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _rigidbody.AddTorque(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _rigidbody.AddTorque(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody.AddTorque(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody.AddTorque(0, 0, -1);
+        }
+        if(this.transform.position.y < -10)
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+            transform.position = new Vector3(0, 2, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /*
+        var item = collision.gameObject.GetComponent<item>();
+        if(item != null)
+
+          if(collision.gameObject.name == "Item")
+          {
+              Destroy(collision.gameObject);
+          }
+       */
+         
+        Debug.Log("衝突" + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var item = other.gameObject.GetComponent<item>();
+        if (item != null)
+        {
+            // Debug.Log("トリガー" + other.gameObject.name);
+            Destroy(other.gameObject);
+        }
+    }
+
+}
